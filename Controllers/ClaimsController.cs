af1efe3c9c203001c2b6e6d9387934afe8322dcb
samiulkarim <karim.51@osu.edu>
2017-03-06 19:28:12 -0500

    Final Changes

--- Controllers/ClaimsController.cs
+++ Controllers/ClaimsController.cs
@@ -10,40 +10,57 @@ using FisherInsuranceApi.Models;
  [HttpPost]
  public IActionResult Post([FromBody] Claim claim)
  {
- return Ok(db.CreateClaim(claim));
+     var newClaim = db.Claims.Add(claim);
+     db.SaveChanges();
+     return CreatedAtRoute("GetClaim", new { id = claim.Id }, claim);
  }
 
 // GET api/auto/claims/5
- [HttpGet("{id}")]
+ [HttpGet("{id}", Name = "GetClaim")]
  public IActionResult Get(int id)
  {
- return Ok(db.RetrieveClaim(id));
+   return Ok(db.Claims.Find(id));
  }
 
  // PUT api/auto/claims/id
  [HttpPut("{id}")]
  public IActionResult Put(int id, [FromBody] Claim claim)
  {
- return Ok(db.UpdateClaim(claim));
+      var newClaim = db.Claims.Find(id);
+      if (newClaim == null)
+    {
+        return NotFound();
+    }
+    newClaim = claim;
+    db.SaveChanges();
+    return Ok(newClaim);
  }
 
 // DELETE api/auto/claims/id
  [HttpDelete("{id}")]
- public IActionResult Delete(int id, [FromBody] Claim claim)
+ public IActionResult Delete(int id)
  {
-    db.DeleteClaim(id);
-    return Ok(); 
+ var claimToDelete = db.Claims.Find(id);
+ if (claimToDelete == null)
+ {
+ return NotFound();
+ }
+ db.Claims.Remove(claimToDelete);
+ db.SaveChangesAsync();
+ return NoContent();
  }
 
- private IMemoryStore db;
-public ClaimsController(IMemoryStore repo)
+ private readonly FisherContext db;
+public ClaimsController(FisherContext context)
 {
- db = repo;
+ db = context;
 } 
 [HttpGet]
 public IActionResult GetClaims()
 {
- return Ok(db.RetrieveAllClaims);
+ return Ok(db.Claims);
 }
+
+
 
  }
--- Data/FisherContext.cs
+++ Data/FisherContext.cs
@@ -4,11 +4,13 @@ namespace FisherInsuranceApi.Data
 {
  public class FisherContext : DbContext
  {
-     public DbSet<Claim> Claims { get; set; }
+
      protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
  {
  string connection = "Data Source = C:/Users/User/3610/FisherInsuranceApi/Fisher_Insurance.sqlite";
  optionsBuilder.UseSqlite(connection);
  }
+      public DbSet<Claim> Claims { get; set; }
+      public DbSet<Quote> Quotes { get; set; }
  }
 }
