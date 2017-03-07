7bdccb54c3b003dbdaf365dc51fc020ca8534333
samiulkarim <karim.51@osu.edu>
2017-03-06 19:45:55 -0500

    Added Dbset for Quote

--- Data/FisherContext.cs
+++ Data/FisherContext.cs
@@ -1,11 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using FisherInsuranceApi.Models;
-namespace FisherInsuranceApi.Data
-{
- public class FisherContext : DbContext
- {
 
-     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+namespace FisherInsuranceApi.Data {
+public class FisherContext : DbContext {
+ protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
  {
  string connection = "Data Source = C:/Users/User/3610/FisherInsuranceApi/Fisher_Insurance.sqlite";
  optionsBuilder.UseSqlite(connection);
