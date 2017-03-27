33f646dbc62fc4af0b2ebf5cffbcd8a155ed5720
samiulkarim <karim.51@osu.edu>
2017-03-27 18:03:40 -0400

    Built Startup.cs

--- Data/FisherContext.cs
+++ Data/FisherContext.cs
@@ -1,14 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using FisherInsuranceApi.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
-namespace FisherInsuranceApi.Data {
-public class FisherContext : DbContext {
- protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
- {
- string connection = "Data Source = C:/Users/User/3610/FisherInsuranceApi/Fisher_Insurance.sqlite";
- optionsBuilder.UseSqlite(connection);
- }
-      public DbSet<Claim> Claims { get; set; }
-      public DbSet<Quote> Quotes { get; set; }
- }
+
+namespace FisherInsuranceApi.Data
+{
+    public class FisherContext : IdentityDbContext<ApplicationUser>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connection = "Data Source = C:/Users/User/3610/FisherInsuranceApi/Fisher_Insurance.sqlite";
+            optionsBuilder.UseSqlite(connection);
+        }
+
+        public DbSet<Claim> Claims { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+    }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,13 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using FisherInsuranceApi.Data;
+using Microsoft.IdentityModel.Tokens;
+using FisherInsuranceApi.Security;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace FisherInsuranceApi
 {
@@ -28,9 +27,20 @@ namespace FisherInsuranceApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
-        services.AddDbContext<FisherContext>();
-        services.AddMvc();
+
+            services.AddMvc();
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(config =>
+            {
+                config.User.RequireUniqueEmail = true;
+                config.Password.RequireNonAlphanumeric = false;
+                config.Cookies.ApplicationCookie.AutomaticChallenge = false;
+            })
+            .AddEntityFrameworkStores<FisherContext>()
+            .AddDefaultTokenProviders();
+
+            services.AddDbContext<FisherContext>();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +51,30 @@ namespace FisherInsuranceApi
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
+
+            //add our JwtProvider that we created
+            app.UseJwtProvider();
+
+            //add the built in authentication 
+            app.UseJwtBearerAuthentication(new JwtBearerOptions()
+            {
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true,
+                RequireHttpsMetadata = false,
+                TokenValidationParameters = new TokenValidationParameters()
+                {
+                    IssuerSigningKey = JwtProvider.SecurityKey,
+                    ValidIssuer = JwtProvider.Issuer,
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                }
+            });
+
+            //if we wanted to support cookies, we do this:
+            //app.UseIdentity();
+            //or this
+            //app.UseCookieAuthentication();
 
             app.UseMvc();
         }
